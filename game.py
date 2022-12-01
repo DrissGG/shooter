b8f6caa5980d5d224dc32f2ede393ce3cb45c808
DRISS <drissgazzaoui1986@hotmail.com>
2022-12-02 00:51:10 +0100

    ajout function check_collision + engendrement du montr& C:/Users/Driss/AppData/Local/Programs/Python/Python311/python.exe e:/COURS/Projet/Jeux2D/Shooter/main.pye

--- game.py
+++ game.py
@@ -1,8 +1,27 @@
 from player import Player
+from monster import Monster
+
+import pygame
+
+
 
 # creer une seconde classe qui va representer notre jeu
 class Game:
+    
     def __init__(self):
         # generer notre joueur 
-         self.player = Player()
-         self.pressed ={}
+        self.all_players = pygame.sprite.Group()
+        self.player = Player(self)
+        self.all_players.add(self.player)
+        #  group de monstre 
+        self.all_monsters = pygame.sprite.Group()
+        self.pressed ={}
+        self.spawn_monster()
+
+    def check_collisioon(self, sprite, group):
+        return pygame.sprite.spritecollide(sprite, group, False, pygame.sprite.collide_mask)
+
+   
+    # def spawn_monster(self):
+    #     monster = Monster(self)
+    #     self.all_monsters.add(monster)
