653a7f23837e9f459fec7b151f379751b63548fa
DRISS <drissgazzaoui1986@hotmail.com>
2022-12-02 00:46:43 +0100

    ajout collision avec un monstres

--- player.py
+++ player.py
@@ -6,12 +6,13 @@ from projectile import Projectile
 
 # creer une premiere classe qui va representer notre joueur 
 class Player(pygame.sprite.Sprite):
-    def __init__(self):
+    def __init__(self, game):
         super().__init__()
+        self.game = game
         self.health = 100
         self.max_health = 100
         self.attack = 10
-        self.velocity = 2
+        self.velocity = 5
         self.all_projectiles = pygame.sprite.Group()
         self.image = pygame.image.load('assets/player.png')
         self.rect = self.image.get_rect()
@@ -24,7 +25,10 @@ class Player(pygame.sprite.Sprite):
 
 
     def move_right(self):
-        self.rect.x += self.velocity
+        # si le jouer nes pas en collision avec un monstre
+        if not self.game.check_collisioon(self, self.game.all_monsters):
+            self.rect.x += self.velocity
+
 
     def move_left(self):
          self.rect.x -= self.velocity
